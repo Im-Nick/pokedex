5372072044938cc2c9364177750dcdd68883c740
im-nick <ddjnick03@gmail.com>
2022-10-24 11:50:56 +0200

    feat: Add external toml file

--- src/settings/mod.rs
+++ src/settings/mod.rs
@@ -1,8 +1,15 @@
-use config::{Config, ConfigError};
+use config::{Config, ConfigError, File};
 use serde::Deserialize;
+#[derive(Deserialize, Debug, Clone)]
+pub struct Api {
+    pub pokemon_api: String,
+    pub yoda_api: String,
+    pub shakespeare_api: String,
+}
 
 #[derive(Deserialize, Debug, Clone)]
 pub struct Settings {
+    pub api: Api,
     pub host: String,
     pub port: u16,
 }
@@ -10,6 +17,7 @@ pub struct Settings {
 impl Settings {
     pub(crate) fn new() -> Result<Self, ConfigError> {
         let builder = Config::builder()
+            .add_source(File::with_name("config/default"))
             // Use env file
             .add_source(config::Environment::with_prefix("APP"))
             .build()?;
